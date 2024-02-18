906ea1cfa956aed9aef947fb9e3c2015578b128b
alice <aliceanicio@hotmail.com>
2024-02-18 18:04:16 -0300

    feat: inserção de formulário de cadastro

--- projeto/src/app/app.module.ts
+++ projeto/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FormCadastroComponent } from './components/form-cadastro/form-cadastro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FormCadastroComponent
   ],
   imports: [
     BrowserModule,
